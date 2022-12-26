280eae5193b2678996f9a15398ceca43833e3c6e
thaisperlho <carvalho.thais0301@gmail.com>
2022-12-26 11:17:51 -0300

    update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+
 from fastapi import FastAPI
 
 from app.resource import news
 
-app = FastAPI(title="Rest API", description="Thais Carvalho")
 
-news.init_app(app=app)
+def create_app():
+    app = FastAPI(title="Rest API", description="Thais Carvalho")
+    news.init_app(app=app)
+    return app
+
+
+if __name__ == "app.main":
+    app = create_app()
